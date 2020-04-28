c0e6ec4b64f7cae328ffa3682216485817b13bed
Klayver Ximenes <klayverx@gmail.com>
2020-04-28 12:02:31 -0300

    first commit

--- exercicios-js/fundamentos/logicos.js
+++ exercicios-js/fundamentos/logicos.js
@@ -1,11 +1,18 @@
-function compras(trabalho1, trabalho2) {
+/**
+ * Possui dois trabalhos
+ * compra tv de 50 e toma sorvete se os dois derem certo
+ * compra tv de 32 e toma sorvete se um dos dois derem certo
+ * nao compra nada nem toma sorvete se os dois derem errado e fica saudável
+ */
+function compras (trabalho1, trabalho2){
     const comprarSorvete = trabalho1 || trabalho2
     const comprarTv50 = trabalho1 && trabalho2
-    // const comprarTv32 = !!(trabalho1 ^ trabalho2) // bitwise xor
+    //const comprarTv32 = !!(trabalho1 ^ trabalho2) // bitwise xor
     const comprarTv32 = trabalho1 != trabalho2
-    const manterSaudavel = !comprarSorvete // operador unário
-
+    const manterSaudavel = !comprarSorvete   //operador unário
+    
     return { comprarSorvete, comprarTv50, comprarTv32, manterSaudavel }
+    //naõ precisa repassar o (chave/valor), repassa automaticamente com objetos
 }
 
 console.log(compras(true, true))
---
+++ exercicios-js/fundamentos/relacionais.js
@@ -0,0 +1,26 @@
+/**
+ * Estritamente igual (===)
+ * leva em conta o valor e o tipo da variável
+ * 
+ * Igual (==)
+ * Compara apenas o valor da variável em questão
+ */
+
+console.log('01)', '1' == 1)  //o tipo nao importa]
+console.log('02)', '1' === 1)  //estritamente igual, onde o tipo importa
+console.log('03)', '3' != 3)
+console.log('04)', '3' !== 3)
+
+console.log('05)', 3 < 2)
+console.log('06)', 3 > 2)
+console.log('07)', 3 <= 2)
+console.log('08)', 3 >= 2)
+
+const d1 = new Date(0)   //1970, data inicial marco 0
+const d2 = new Date(0)
+console.log('09)', d1 === d2)   //variaveis com referencias de memória
+console.log('10)', d1 == d2)
+console.log('11)', d1.getTime() === d2.getTime())  //comparação de number, data em milisegundos
+
+console.log('12)', undefined == null)
+console.log('13)', undefined === null)
