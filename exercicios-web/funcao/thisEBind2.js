a2cdd981aad71a8e8b30073e6d2d37952c132056
Klayver Ximenes <klayverx@gmail.com>
2020-04-30 11:30:40 -0300

    first commit

--- exercicios-web/funcao/thisEBind2.js
+++ exercicios-web/funcao/thisEBind2.js
@@ -1,10 +1,12 @@
 function Pessoa () {
     this.idade = 0   // criado com o this para que possa ser manipulado
 
+    const self = this
+    
     setInterval(function() {   // dispara outra função com determinado intervalo
-        this.idade++
-        console.log(this.idade)
-    }.bind(this), 1000)   // chamadas da função no intervalo que for passado em milissegundos
+        self.idade++
+        console.log(self.idade)
+    }/*.bind(this)*/, 1000)   // chamadas da função no intervalo que for passado em milissegundos
 }
 
 new Pessoa
