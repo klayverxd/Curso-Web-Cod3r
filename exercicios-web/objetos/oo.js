0e0682efbdfcffc3c9b6bbc15ed1039ab3f5c60c
Klayver Ximenes <klayverx@gmail.com>
2020-05-05 17:01:39 -0300

    first commit

---
+++ exercicios-web/objetos/oo.js
@@ -0,0 +1,28 @@
+/**
+ * CÓDIGO NÃO EXECUTÁVEL!!!
+*/
+
+// Procedural
+// Funções que manipulam dados
+processamento(valor1, valor2, valor3)
+
+// OO
+// Funções contidas dentro de dados
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        // ...
+    }
+}
+
+objeto.processamento()   // Foco passou a ser o objeto
+
+/**
+ * Princípios importantes:
+ * 1. Abstração
+ * 2. Encapsulamento ('esconder informações')
+ * 3. Herança (prototype)(Recebe características (atributos))
+ * 4. Polimorfismo (Objetos de vários tipos)
+ */
